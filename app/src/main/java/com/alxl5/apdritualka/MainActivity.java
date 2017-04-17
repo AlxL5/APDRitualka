457477773ea594aec830f4f10f1b6bc554ee32ef
AlxL5 <alexandr.minenko@gmail.com>
2017-04-17 17:32:16 +0700

    Add main toolbar

--- app/src/main/java/com/alxl5/apdritualka/MainActivity.java
+++ app/src/main/java/com/alxl5/apdritualka/MainActivity.java
@@ -1,14 +1,21 @@
 package com.alxl5.apdritualka;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v7.widget.Toolbar;
+        import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
     }
 }
