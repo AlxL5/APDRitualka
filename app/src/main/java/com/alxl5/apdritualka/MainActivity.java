179e24bff7ed7fc6391a032d8677e7c69f363bfd
AlxL5 <alexandr.minenko@gmail.com>
2017-04-17 16:53:06 +0700

    Add main activity

---
+++ app/src/main/java/com/alxl5/apdritualka/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alxl5.apdritualka;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
